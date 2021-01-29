68a56b23468f5153fd00adc5c01028fe36094766
David Morales <dsmorales349@misena.edu.co>
2021-01-29 11:15:26 -0500

    add bonus interfaces

---
+++ apps/vakers/src/app/models/vaki.ts
@@ -0,0 +1,11 @@
+export interface Vaki {
+  description: string;
+  start_date: number;
+  close_date: number;
+  name: string;
+  summary: string;
+  url: string;
+  photo?: string;
+  video?: string;
+  //country: Country['isoCode'];
+}
