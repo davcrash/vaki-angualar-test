475fab47f2ca468590bd8cac6ccb7eeda09aac84
David Morales <dsmorales349@misena.edu.co>
2021-01-29 11:11:51 -0500

    add vakers theme

--- apps/vakers/src/app/app.module.ts
+++ apps/vakers/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatSliderModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
