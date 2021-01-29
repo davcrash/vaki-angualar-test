6e746aa3a85b56947cf42255cd79d8bb0c378add
David Morales <dsmorales349@misena.edu.co>
2021-01-29 12:21:29 -0500

    finish vaki rewards style

--- apps/vakers/src/app/app.module.ts
+++ apps/vakers/src/app/app.module.ts
@@ -4,18 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { RewardContainerComponent } from './components/reward-container/reward-container.component';
 import { RewardItemComponent } from './components/reward-item/reward-item.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { VakiContainerComponent } from './components/vaki-container/vaki-container.component';
 
 @NgModule({
-  declarations: [AppComponent, RewardContainerComponent, RewardItemComponent, VakiContainerComponent],
+  declarations: [
+    AppComponent,
+    RewardContainerComponent,
+    RewardItemComponent,
+    VakiContainerComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatGridListModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- apps/vakers/src/app/components/reward-container/reward-container.component.ts
+++ apps/vakers/src/app/components/reward-container/reward-container.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { VakiReward } from '../../models/vaki-reward';
 
 @Component({
   selector: 'vaki-challenge-reward-container',
   templateUrl: './reward-container.component.html',
-  styleUrls: ['./reward-container.component.scss']
+  styleUrls: ['./reward-container.component.scss'],
 })
-export class RewardContainerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class RewardContainerComponent {
+  reward: VakiReward = {
+    title: 'Este es un titulo',
+    key: 'todo',
+    description:
+      'Lorem Ipsum is simply dumm asd asd as das das da asd a qwe d as dqkwdkqwd as dqw asdasdqwe ',
+    img: 'https://material.angular.io/assets/img/examples/shiba1.jpg',
+    value: '$12312 COP',
+    claimed: 1,
+    visible: true,
+    delivery_date: 123,
+    quantityAvailable: 1,
+  };
 }
